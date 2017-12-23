0dfe395ad497f0c081dff2a922b38f3cceee19c6
Fernando Méndez <mendezc.jf@gmail.com>
2017-12-23 17:45:16 -0600

    Adding enums and deleting unused controller

---
+++ Betcoins.Model/Common/BetcoinsEnum.cs
@@ -0,0 +1,25 @@
+﻿namespace Betcoins.Model.Common
+{
+    public enum Competition
+    {
+        WorldCup = 1
+    }
+
+    public enum MatchType
+    {
+        Classification = 1,
+        Eliminatory,
+        Friendly,
+        Final
+    }
+
+    public enum Phase
+    {
+        Groups = 1,
+        RoundOf16,
+        QuarterFinal,
+        SemiFinal,
+        MatchFor3RdPlace,
+        Final
+    }
+}
