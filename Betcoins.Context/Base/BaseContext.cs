07f16557b59c17e82fe256f0edf9d0351c5cbb2f
Fernando Méndez <mendezc.jf@gmail.com>
2017-12-23 23:22:02 -0600

    Adding base context of application

---
+++ Betcoins.Context/Base/BaseContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+
+namespace Betcoins.Context.Base
+{
+    public class BaseContext<TContext> : DbContext where TContext : DbContext
+    {
+        static BaseContext()
+        {
+            Database.SetInitializer<TContext>(null);
+        }
+
+        protected BaseContext() : base("name=BetcoinsConnection")
+        {
+        }
+    }
+}
---
+++ Betcoins.Context/Common/AccountContext.cs
@@ -0,0 +1,17 @@
+﻿using Betcoins.Context.Base;
+using Betcoins.DomainClasses.Models;
+using Betcoins.DomainClasses.Models.Mapping;
+using System.Data.Entity;
+
+namespace Betcoins.Context.Common
+{
+    public class AccountContext : BaseContext<AccountContext>
+    {
+        public DbSet<Account> Accounts { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new AccountMap());
+        }
+    }
+}
