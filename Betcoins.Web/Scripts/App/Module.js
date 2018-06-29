d6cf765cbb08be63938ea773e15895ae9964d916
Fernando Méndez <mendezc.jf@gmail.com>
2018-06-29 18:02:58 -0500

    Adding login service for Google

--- Betcoins.Web/Scripts/App/Module.js
+++ Betcoins.Web/Scripts/App/Module.js
@@ -39,7 +39,7 @@
                 domain: webConfig.auth0Domain,
                 clientID: webConfig.auth0ClientId,
                 options: {
-                    allowedConnections: ['facebook', 'github', 'twitter'],
+                    allowedConnections: ['facebook', 'twitter', 'github', 'google-oauth2'],
                     allowSignUp: false,
                     container: 'auth0-login-container',
                     auth: {
